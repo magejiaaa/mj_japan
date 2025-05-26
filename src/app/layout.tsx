ef9ec4ea9b58e0be28134611f0f646dce02badd3
jenny <jenny@ewebs.com.tw>
2025-05-26 17:00:11 +0800

    更新頁面元數據描述，新增 Google 站點驗證資訊

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,11 @@ import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: '秘境自助報價',
-  description: 'Created with v0',
+  description: '協助用戶快速估算從日本購買商品的總費用',
   generator: '秘境日本代購',
+  other: {
+    'google-site-verification': 'LctgswFN5j-wK806zPBsuGYxy_hrpZmkP9DRlirlcYk'
+  },
 }
 
 export default function RootLayout({
